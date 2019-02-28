428f503090f5abbae55e0834d2c96ce9df4cbf62
Clems <c.fernandez@lilo.org>
2019-02-28 19:37:22 +0100

    Test 2

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Articles;
+use App\Repository\ArticlesRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Form\ArticleType;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticlesRepository $repo)
+    {
+        $articles = $repo->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles'        => $articles
+        ]);
+    }
+
+    /**
+    * @Route("/", name="home")
+    **/
+    public function home()
+    {
+      return $this->render('blog/home.html.twig', [
+        'title' => 'Bienvenue',
+        'age' => '31'
+      ]);
+    }
+
+    /**
+    * @Route("/blog/new", name="blog_create")
+    * @Route("/blog/{id}/edit", name="blog_edit")
+    **/
+    public function form(Articles $article = null, Request $request, ObjectManager $manager)
+    {
+      if(!$article)
+      {
+        $article = new Articles();
+      }
+
+      $form = $this->createForm(ArticleType ::class, $article);
+
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid())
+      {
+        if (!$article->getId())
+        {
+          $article->setCreatedAt(new \DateTime());
+        }
+        $manager->persist($article);
+        $manager->flush();
+
+        return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+      }
+
+      return $this->render('blog/create.html.twig', [
+        'formArticle' => $form->createView(),
+        'editMode'  => $article->getId() !== null
+      ]);
+    }
+
+    /**
+    * @Route("/blog/{id}", name="blog_show")
+    **/
+    public function show(Articles $article)
+    {
+      return $this->render('blog/show.html.twig', [
+        'article' => $article
+      ]);
+    }
+
+
+}
---
+++ src/Entity/Courses.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CoursesRepository")
+ */
+class Courses
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $teachers;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDuration(): ?string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?string $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getTeachers(): ?string
+    {
+        return $this->teachers;
+    }
+
+    public function setTeachers(string $teachers): self
+    {
+        $this->teachers = $teachers;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
