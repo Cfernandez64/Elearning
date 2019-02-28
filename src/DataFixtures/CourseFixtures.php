40573e42003115d929cf6df6b206f8d10249aad4
Clems <c.fernandez@lilo.org>
2019-02-28 19:35:19 +0100

    Test

--- src/DataFixtures/CourseFixtures.php
+++ src/DataFixtures/CourseFixtures.php
@@ -2,63 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Courses;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use App\Entity\Articles;
-use App\Entity\Category;
-use App\Entity\Comment;
 
-class ArticleFixtures extends Fixture
+class CourseFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create('fr_FR');
 
-        //Créer trois catégories fakée
+        //Créer trois cours fakés
         for ($i = 0; $i <= 3; $i++)
         {
-          $category = new Category();
-          $category->setTitle($faker->sentence())
-                  ->setDescription($faker->paragraph());
-          $manager->persist($category);
-
-          for ($j = 1; $j<= mt_rand(4,6); $j++)
-          {
-            $article = new Articles();
-
-            $content = '<p>';
-            $content .= join($faker->paragraphs(5), '</p><p>');
-            $content .= '</p>';
-
-            $article->setTitle($faker->sentence())
-                    ->setContent($content)
-                    ->setImage($faker->imageUrl())
-                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
-                    ->setCategory($category);
-
-            $manager->persist($article);
-
-            for ($k = 1; $k <= mt_rand(4,10); $k++)
-            {
-              $content = '<p>';
-              $content .= join($faker->paragraphs(2), '</p><p>');
-              $content .= '</p>';
-
-              $now = new \DateTime();
-              $interval = $now->diff($article->getCreatedAt());
-              $days = $interval->days;
-              $minimum = '-'.$days.'days';
-
-              $comment = new Comment();
-              $comment->setAuthor($faker->name())
-                      ->setContent($content)
-                      ->setCreatedAt($faker->dateTimeBetween($minimum))
-                      ->setArticle($article);
-              $manager->persist($comment);
-            }
-          }
+          $course = new Courses();
+          $course->setTitre($faker->sentence())
+                 ->setDuration($faker->sentence(3))
+                 ->setTeachers($faker->name())
+                 ->setDescription($faker->paragraph(4));
+          $manager->persist($course);
         }
-
         $manager->flush();
     }
 }
+
